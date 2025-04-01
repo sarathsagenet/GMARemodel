8009443f203faf000c147713281121eee83b0c6c
sarathsagenet <81241617+sarathsagenet@users.noreply.github.com>
2025-04-01 11:27:55 -0400

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-function showImage(type) {
-    const imageMap = {
-      old: 'images/OldArchitecture_PNG.png',
-      current: 'images/current.png',
-      new: 'images/new.png'
-    };
-    document.getElementById('archImage').src = imageMap[type];
-  }
-  
